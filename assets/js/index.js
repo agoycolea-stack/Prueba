bdeb89a7124a0833923f631f9a7c2f4dfbc9621b
Agustin Goycolea <agoycolea@fen.uchile.cl>
2020-05-07 21:02:01 -0400

    Estamos casi, agregamos todas las secciones, me falta acomodar H1,footer y navbar estatico

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+    $("a").click(function(e){
+      if (this.hash !== ""){
+        e.preventDefault();
+        var url = this.hash;
+        $('html,body').animate({
+          scrollTop: $(url).offset().top
+        }, 800, function(){
+          window.location.hash = url;
+        });
+      }
+    });
+});
